dc70155ff64c0aece56930a21d57de152c942a74
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-08 17:31:05 +0200

    fix: remove outdated updater files and documentation

--- updater/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Main package
-"""
-
-# Make imports easier
-from .services.updater_service import UpdaterService
-from .models.app_updater import AppUpdater
-from .ui.updater_gui import UpdaterGUI
