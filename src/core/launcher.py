bbec47db63161fa364572bbab07bdfb7cec9345d
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 07:32:00 +0200

    refactor: streamline Launcher start process and enhance loader update handling

--- src/core/launcher.py
+++ src/core/launcher.py
@@ -22,12 +22,13 @@ class Launcher:
         # self.instance_service = InstanceService()
 
     def start(self):
-        self.ui_service.show(Window.MAIN)
+        self.main_window = self.ui_service.show(Window.MAIN)
         self._check_launcher_update()
+        self._set_up_profile()
+        self._check_loader_update()
         # self._check_for_updates()
 
     def update(self):
-        self._set_up_profile()
         logging.info("Updating...")
 
     def launch(self):
@@ -46,6 +47,13 @@ class Launcher:
         else:
             logging.info("Launcher is up to date.")
 
+    def _check_loader_update(self):
+        if self.version_service.check_for_updates("loader"):
+            self.main_window.launch_button.text = "Update"
+            logging.info("Loader update available, updating...")
+        else:
+            logging.info("Loader is up to date.")
+
     def _set_up_profile(self):
         logging.info("Setting up profile...")
         self.profile = self.profile_service.get_profile_data()
@@ -56,15 +64,3 @@ class Launcher:
             logging.warning("No profile found, creating a new one")
             self.profile_service.update_profile()
         self.profile_service.add_profile_to_instance()
-
-    # def _check_for_updates(self):
-    #     logging.info("Checking for updates")
-    #     current_versions = self.instance_service.get_current_versions()
-    #     if current_versions:
-    #         logging.debug("Current versions: %s", current_versions)
-    #         self.github_service.check_for_updates(current_versions)
-    #     else:
-    #         logging.warning("Could not retrieve current versions")
-
-    # def _set_up_neoforge(self):
-    #     logging.info("Setting up NeoForge")
