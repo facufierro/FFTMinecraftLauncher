666a0ffa8e69379dd006c537d840607f82c7f608
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 06:53:33 +0200

    refactor: enhance UpdateDialog to emit signal on accept and improve exit logging

--- src/ui/components/update_dialog.py
+++ src/ui/components/update_dialog.py
@@ -1,8 +1,12 @@
 import logging
+import sys
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtCore import pyqtSignal
 
 
 class UpdateDialog(QDialog):
+    accept_pressed = pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Update Available")
@@ -11,4 +15,10 @@ class UpdateDialog(QDialog):
         layout.addWidget(self.label)
         self.accept_button = QPushButton("Accept")
         layout.addWidget(self.accept_button)
+        self.accept_button.clicked.connect(self.on_accept_pressed)
         self.setLayout(layout)
+
+    def on_accept_pressed(self):
+        self.accept_pressed.emit()
+        logging.info("Exiting launcher...")
+        sys.exit()
