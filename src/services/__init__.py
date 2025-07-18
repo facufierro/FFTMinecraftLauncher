8db768a0b978b873af73ffbe1f3701fc777fab82
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 06:15:31 +0200

    refactor: remove unused service initializations from __init__.py

--- src/services/__init__.py
+++
@@ -1,7 +0,0 @@
-from .github_service import GitHubService
-from .version_service import VersionService
-from .launcher_service import LauncherService
-
-github_service = GitHubService()
-version_service = VersionService()
-launcher_service = LauncherService()
