302cfefb1f80a242df61ca0ed62e3825b6e3ff6d
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 12:36:17 +0200

    refactor: update import statements for Path and File in Instance class

--- src/models/instance.py
+++ src/models/instance.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Optional, List, Dict
-from ..models.constants import Paths
+from ..models.constants import Path, File
 
 
 @dataclass
@@ -8,8 +8,8 @@ class Instance:
     name: str
     current_versions: Dict[str, str] = field(default_factory=dict)
     required_versions: Dict[str, str] = field(default_factory=dict)
-    instance_path: str = Paths.INSTANCE_DIR.value
-    versions_file: str = Paths.VERSIONS_FILE.value
+    instance_path: str = Path.INSTANCE_DIR.value
+    versions_file: str = File.VERSIONS.value
     mod_list: List[str] = field(default_factory=list)
     resourcepacks: List[Dict[str, str]] = field(default_factory=list)
     shaders: List[Dict[str, str]] = field(default_factory=list)
