75efb6ce91f2d84686c1e68f012c17e9a0149c55
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 04:54:42 +0200

    feat: implement update checking logic in VersionService

--- src/services/version_service.py
+++ src/services/version_service.py
@@ -40,6 +40,26 @@ class VersionService:
                 logging.error("Error decoding JSON from GitHub: %s", e)
         return {None, None, None}
 
+    def check_for_updates(self, content):
+        try:
+            github_versions = self.get_github_versions(content)
+            current_versions = self.get_current_versions()
+            launcher_missmatch = (
+                current_versions["launcher"] != github_versions["launcher"]
+            )
+            loader_missmatch = current_versions["loader"] != github_versions["loader"]
+            minecraft_missmatch = (
+                current_versions["minecraft"] != github_versions["minecraft"]
+            )
+            return {
+                "launcher": launcher_missmatch,
+                "loader": loader_missmatch,
+                "minecraft": minecraft_missmatch,
+            }
+        except Exception as e:
+            logging.error(f"Error checking for updates: {e}")
+            return {"launcher": False, "loader": False, "minecraft": False}
+
     def _extract_version(self, version_string):
         """
         Extracts the version number from a version string.
