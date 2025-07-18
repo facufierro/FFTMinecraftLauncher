a21a7dc0386d6278bc91312f04ba0adba8b2ca26
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-18 11:40:00 +0200

    refactor: simplify InstanceService initialization and remove unused version management logic

--- src/services/instance_service.py
+++ src/services/instance_service.py
@@ -1,20 +1,14 @@
-# handles instance operations, including creation, deletion, and management of Minecraft instances.
-
 import logging
 import os
-import json
-
-from ..models.constants import Paths
+import shutil
+from ..models.instance import Instance
 
 
 class InstanceService:
-    def __init__(self):
+    def __init__(self, instance: Instance):
         logging.debug("Initializing InstanceService")
-        self.instance_path = Paths.INSTANCE_DIR.value
-        self.version_file = f"{self.instance_path}/versions.json"
-        self.launcher_version, self.loader_version, self.minecraft_version = (
-            self.get_current_versions()
-        )
+        self.instance = instance
+        self.instance_path = self.instance.instance_path
         try:
             self.create_instance_folder()
 
@@ -30,42 +24,11 @@ class InstanceService:
             logging.error("Failed to create instance folder: %s", e)
             raise
 
-    def delete_instance(self):
-        import shutil
-
+    def delete_instance_folder(self):
         logging.info("Deleting the instance")
         try:
             if os.path.exists(self.instance_path):
                 shutil.rmtree(self.instance_path)
         except Exception as e:
             logging.error("Failed to delete instance: %s", e)
-            raise
-
-    def get_current_versions(self):
-        logging.info("Retrieving current versions")
-        if not os.path.exists(self.version_file):
-            logging.warning("Versions file not found, using default versions")
-            return {"launcher": "0.0.0", "loader": "0.0.0", "minecraft": "0.0.0"}
-        try:
-            # get versions from the versions.json file
-            if os.path.exists(self.version_file):
-                with open(self.version_file, "r") as f:
-                    data = json.load(f)
-                    return {
-                        "launcher": data["launcher"],
-                        "loader": data["loader"],
-                        "minecraft": data["minecraft"],
-                    }
-                logging.info(
-                    "Current versions retrieved: Launcher: %s, Loader: %s, Minecraft: %s",
-                    data["launcher"],
-                    data["loader"],
-                    data["minecraft"],
-                )
-            else:
-                logging.warning("Versions file not found, returning default versions")
-                return {"launcher": None, "loader": None, "minecraft": None}
-
-        except Exception as e:
-            logging.error("Failed to retrieve current versions: %s", e)
             raise
