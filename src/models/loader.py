440b4a1bb881a690372ba6067c6b7e635895200e
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-24 19:59:38 +0200

    feat: refactor Loader class to streamline version handling and file path initialization

--- src/models/loader.py
+++ src/models/loader.py
@@ -1,20 +1,31 @@
 import os
 from dataclasses import dataclass, field
+from ..version import __loader_version__
 
 
 @dataclass
 class Loader:
+    minecraft_dir: str
     downloads_dir: str
-    current_version: str = field(default=None)
-    required_version: str = field(default=None)
-    file_name: str = field(init=False)
+    required_version: str = __loader_version__
     download_url: str = field(init=False)
-    existing_file: str = field(init=False)
+    installer: str = field(init=False)
+    launcher: str = field(init=False)
 
     def __post_init__(self):
-        self.file_name = f"neoforge-{self.required_version}-installer.jar"
         self.download_url = (
             f"https://maven.neoforged.net/releases/net/neoforged/neoforge/"
             f"{self.required_version}/neoforge-{self.required_version}-installer.jar"
         )
-        self.existing_file = os.path.join(self.downloads_dir, self.file_name)
+        self.installer = os.path.join(
+            self.downloads_dir, f"neoforge-{self.required_version}-installer.jar"
+        )
+        self.launcher = os.path.join(
+            self.minecraft_dir,
+            "libraries",
+            "net",
+            "neoforged",
+            "neoforge",
+            self.required_version,
+            f"neoforge-{self.required_version}-client.jar",
+        )
