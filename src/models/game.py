a7e3b0b9671b474636c1fcbad69eddfc57d4b02c
Facundo Fierro <fierrof_47@hotmail.com>
2025-07-26 09:38:15 +0200

    fix: update manifest URL to the latest version for game data retrieval

--- src/models/game.py
+++ src/models/game.py
@@ -4,4 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class Game:
     version: str = "1.21.1"
-    manifest_url: str = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+    manifest_url: str = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
